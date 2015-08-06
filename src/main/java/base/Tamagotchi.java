7fa157cd08b70060081bbbfcefb6fd83647c4d91
NikitaSamoilov <nswork1987@gmail.com>
2015-08-06 16:16:01 +0300

    Initial commit

---
+++ src/main/java/base/Tamagotchi.java
@@ -0,0 +1,53 @@
+package base;
+
+import java.math.BigInteger;
+
+public class Tamagotchi {
+    protected int maxSatiety;
+    protected int maxComfort;
+    protected int maxJoy;
+    protected int adequeteSatiety;
+    protected int adequeteComfort;
+    protected int adequeteJoy;
+    protected int satietyPortion;
+    protected int satiety;
+    protected int comfort;
+    protected int joy;
+
+    public int getMaxSatiety() {
+        return maxSatiety;
+    }
+
+    public int getMaxComfort() {
+        return maxComfort;
+    }
+
+    public int getMaxJoy() {
+        return maxJoy;
+    }
+
+    public int getAdequeteSatiety() {
+        return adequeteSatiety;
+    }
+
+    public int getAdequeteComfort() {
+        return adequeteComfort;
+    }
+
+    public int getAdequeteJoy() {
+        return getAdequeteJoy();
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public int getComfort() {
+        return comfort;
+    }
+
+    public int getJoy() {
+        return joy;
+    }
+
+}
